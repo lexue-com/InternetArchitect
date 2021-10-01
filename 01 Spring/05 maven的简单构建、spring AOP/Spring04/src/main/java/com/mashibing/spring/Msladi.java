593df2b473fe6e062208d8328e454c37a4ad430d
lxyspotless <lxyspotless@163.com>
2021-10-02 00:33:13 +0800

    101笔记更新

---
+++ 01 Spring/05 maven的简单构建、spring AOP/Spring04/src/main/java/com/mashibing/spring/Msladi.java
@@ -0,0 +1,15 @@
+package com.mashibing.spring;
+
+public class Msladi implements Car {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "玛莎拉蒂";
+	}
+
+	public String getPrice() {
+		// TODO Auto-generated method stub
+		return "120w";
+	}
+
+}
