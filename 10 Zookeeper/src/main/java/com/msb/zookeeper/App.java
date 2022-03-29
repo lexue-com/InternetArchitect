c0114ae91440e5ceaba3ed1231395a2f83867ab4
lxyspotless <lxyspotless@163.com>
2022-03-29 23:36:25 +0800

    0329 复盘

--- 10 Zookeeper/src/main/java/com/msb/zookeeper/App.java
+++ 10 Zookeeper/src/main/java/com/msb/zookeeper/App.java
@@ -6,22 +6,24 @@ import org.apache.zookeeper.data.Stat;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * Hello world!
- *
+ * zk是有session概念的，没有连接池的概念
+ *watch:观察/回调/监听
+ * watch的注册值发生在 读类型调用，get，exites。。。
+ * Watch的回调方法：
+ *      场景一：new zk 时候，传入的watch，这个watch是session级别的，跟path 、node没有关系。
+ *      场景二：建立好连接后的实际使用，这个watch是针对path的，是一次性有效的，要永久生效得重复注册
  */
-public class App 
+public class App
 {
     public static void main( String[] args ) throws Exception {
         System.out.println( "Hello World!" );
 
-        //zk是有session概念的，没有连接池的概念
-        //watch:观察，回调
-        //watch的注册值发生在 读类型调用，get，exites。。。
-        //第一类：new zk 时候，传入的watch，这个watch，session级别的，跟path 、node没有关系。
+        //第一类：new zk 时候，传入的watch，这个watch是session级别的，跟path 、node没有关系。
         final CountDownLatch cd = new CountDownLatch(1);
+        //new的对象瞬间返回，但是session的创建及连接zk是异步完成的，所以用CountDownLatch倒数计数器锁可以避免连接未完成导致对象不可用
         final ZooKeeper zk = new ZooKeeper("192.168.150.11:2181,192.168.150.12:2181,192.168.150.13:2181,192.168.150.14:2181",
-                3000, new Watcher() {
-            //Watch 的回调方法！
+                3000, new Watcher() {   //session的过期时间决定了ephemeral临时节点的生效时间
+            //场景一：new zk 时候，传入的watch，这个watch是session级别的，跟path 、node没有关系。
             @Override
             public void process(WatchedEvent event) {
                 Event.KeeperState state = event.getState();
@@ -38,6 +40,7 @@ public class App
                         break;
                     case SyncConnected:
                         System.out.println("connected");
+                        // 使当前线程在锁存器倒计数至零之前一直等待，除非线程被中断。
                         cd.countDown();
                         break;
                     case AuthFailed:
@@ -87,18 +90,20 @@ public class App
             case NOT_CONNECTED:
                 break;
         }
-
-
+        //添加数据
         String pathName = zk.create("/ooxx", "olddata".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-
+        //1、同步获取数据：调用之后阻塞获取数据    此时getData()返回值类型位为byte[]
+          //getData (String path,Watcher watch,Stat stat)    --watch：针对path的回调 传stat返回的是源数据（包括事务ID等），不传返回的是节点上1M那个数据
+          //getData (String path,boolen watch,Stat stat)  --boolen值传false:只是单纯取数据 true:代表DefaultWatch被重新注册，指new zk的那个watch
         final Stat  stat=new Stat();
         byte[] node = zk.getData("/ooxx", new Watcher() {
+            //场景二：建立好连接后的实际使用，取数据的同时注册一个监听，取完数据之后未来这个path发生变化会再次回调这个方法
             @Override
             public void process(WatchedEvent event) {
                 System.out.println("getData watch: "+event.toString());
                 try {
-                    //true   default Watch  被重新注册   new zk的那个watch
-                    zk.getData("/ooxx",this  ,stat);
+、                  //重复注册path的watch：回调处理完继续监听下一次变更
+                    zk.getData("/ooxx",this ,stat);
                 } catch (KeeperException e) {
                     e.printStackTrace();
                 } catch (InterruptedException e) {
@@ -109,11 +114,14 @@ public class App
 
         System.out.println(new String(node));
 
-        //触发回调
+        //“ooxx”被修改会触发回调(getData watch)
         Stat stat1 = zk.setData("/ooxx", "newdata".getBytes(), 0);
-        //还会触发吗？
+        //二次修改还会触发回调吗？如果没有重复注册path监听的话是不会再触发回调的，因为watch是一次性的
         Stat stat2 = zk.setData("/ooxx", "newdata01".getBytes(), stat1.getVersion());
 
+        //2、异步获取：调用之后不阻塞，数据通过回调方法返回  此时getData()返回值类型位为void
+          //getData (String path,Watcher watch,DataCallback cb,Object ctx)    --ctx:上下文
+          //getData (String path,boolen watch,DataCallback cb,Object ctx)
         System.out.println("-------async start----------");
         zk.getData("/ooxx", false, new AsyncCallback.DataCallback() {
             @Override
